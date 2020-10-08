9b6ffe54e5faa4dfbd4d8b59eb96565bb25d1d64
philippepecoraro <philippe.pecoraro@bbox.fr>
2020-10-08 11:18:56 +0200

    Commit initiale

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://phpeco:OpenProjet6@cluster0.vcgf1.mongodb.net/Projet6?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDb réussie !'))
+    .catch(() => console.log('Connexion à MongoDb échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+
+app.use('/api/sauces', stuffRoutes);
+
+
+module.exports = app;
---
+++ routes/stuff.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const stuffCtrl = require('../controllers/stuff');
+
+router.post('/', stuffCtrl.createThing);
+router.post('/:id/like', stuffCtrl.createLike);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.get('/', stuffCtrl.getAllThings);
+
+module.exports = router;
